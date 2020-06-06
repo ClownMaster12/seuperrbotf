1e9dc8eca6ee61cd2257920a5117f1a4cce40cdf
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-06 23:12:11 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -380,7 +380,40 @@ async def unmute_error(ctx, error):
 
 
 
+@client.event
+async def on_guild_remove(guild:discord.Guild):
+  channel = client.get_channel(718950014843355157)
+  embed = discord.Embed(
+        title=f"I've been removed from a guild", description=f"", colour=discord.Colour.red())
+  embed.add_field(name="Name", value=guild.name, inline=True)
+  embed.add_field(name="Member Count", value=len(guild.members), inline=True)
+  embed.add_field(name="Guild Owner", value=guild.owner, inline=True)
+  embed.set_thumbnail(url=guild.icon_url)
+  embed.timestamp = datetime.datetime.utcnow()
 
+  embed2 = discord.Embed(
+        title=f"We now have {len(client.guilds)}", description=f"", colour=discord.Colour.red())
+
+  await channel.send(embed=embed)
+  await channel.send(embed=embed2)
+  
+@client.event
+async def on_guild_join(guild:discord.Guild):
+  channel = client.get_channel(718950014843355157)
+  embed = discord.Embed(
+        title=f"I've been added to a guild", description=f"", colour=discord.Colour.green())
+  embed.add_field(name="Name", value=guild.name, inline=True)
+  embed.add_field(name="Member Count", value=len(guild.members), inline=True)
+  embed.add_field(name="Guild Owner", value=guild.owner, inline=True)
+  embed.set_thumbnail(url=guild.icon_url)
+  embed.timestamp = datetime.datetime.utcnow()
+
+
+  embed2 = discord.Embed(
+        title=f"We now have {len(client.guilds)}", description=f"", colour=discord.Colour.red())
+
+  await channel.send(embed=embed)
+  await channel.send(embed=embed2)
 
 
 

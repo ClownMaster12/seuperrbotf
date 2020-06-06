9f90355ad73ab14f761d839a3779c285659cfb3a
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-06 23:07:30 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -294,6 +294,7 @@ async def mute(ctx, member: discord.Member=None):
         msg = await ctx.send(embed = embed)
         return
     role = discord.utils.get(ctx.guild.roles, name="Muted")
+    role = discord.utils.get(ctx.guild.roles, name="muted")
     await member.add_roles(role)
     embed = discord.Embed(
     title='', description=f'{member.mention} has been muted.' , colour=discord.Colour.green())
@@ -329,6 +330,7 @@ async def unmute(ctx, member: discord.Member=None):
         msg = await ctx.send(embed = embed)
         return
     role = discord.utils.get(ctx.guild.roles, name="Muted")
+    role = discord.utils.get(ctx.guild.roles, name="muted")
     await member.remove_roles(role)
     embed = discord.Embed(
         title='', description=f'{member.mention} has been unmuted.' , colour=discord.Colour.green())

845ac886df95f7977f61ff5336e4ef96bf62cad3
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-07 01:21:11 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -971,6 +971,42 @@ async def mutebroken(ctx):
     msg = await ctx.send(embed = embed)
 
 
+    
+    
+    @client.command()
+async def ping(ctx):
+    try:
+      t = time.time()
+      await ctx.trigger_typing()
+      t2 = round((time.time() - t) * 1000)
+      start = time.perf_counter()
+      embed = discord.Embed(title=f'', description=f'Pinging...', colour=0x2f3136)
+
+
+
+      message = await ctx.send(embed=embed)
+      await message.delete()
+      end = time.perf_counter()
+      duration = (end - start) * 100
+
+
+
+      embed = discord.Embed(
+            title=f'', description='' , colour=0x2f3136)
+
+      embed.add_field(name="Client Ping:", value=f"{round(client.latency * 1000)}ms", inline=False)
+      embed.add_field(name="API Latency:", value=f"{round(duration)}ms", inline=False)
+      embed.add_field(name="Typing Delay:", value=f"{t2}ms", inline=False)
+
+      msg = await ctx.send(embed = embed)
+    except Exception as e:
+        embed = discord.Embed(
+            title=f'The ping command had a error!', description=f'{e}' , colour=0x2f3136)
+
+  
+        msg = await ctx.send(embed = embed)
+    
+    
 
 
 @client.command()
@@ -1093,34 +1129,7 @@ async def unmute_error(ctx, error):
 
 
 
-@client.command()
-async def ping(ctx):
-    try:
-      t = time.time()
-      await ctx.trigger_typing()
-      t2 = round((time.time() - t) * 1000)
-      start = time.perf_counter()
-      embed = discord.Embed(title=f'', description=f'Pinging...', colour=0x2f3136)
 
-
-
-      message = await ctx.send(embed=embed)
-      await message.delete()
-      end = time.perf_counter()
-      duration = (end - start) * 100
-
-
-
-      embed = discord.Embed(
-            title=f'', description='' , colour=0x2f3136)
-
-      embed.add_field(name="Client Ping:", value=f"{round(client.latency * 1000)}ms", inline=False)
-      embed.add_field(name="API Latency:", value=f"{round(duration)}ms", inline=False)
-      embed.add_field(name="Typing Delay:", value=f"{t2}ms", inline=False)
-
-      msg = await ctx.send(embed = embed)
-    except Exception as e:
-        await ctx.send(e)
         
 
 

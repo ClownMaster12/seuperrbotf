0ef3d0527627d20403fd1d4bb935b6dbdb0aa881
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-07 01:10:37 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -1422,10 +1422,6 @@ async def help(ctx):
 
 
 
-@client.command()
-async def ping(ctx):
-    """Sends the clients latency"""
-    await ctx.send(f"Ping! ( `{round(client.latency * 1000)}ms` )")
 
 
 @commands.is_owner()

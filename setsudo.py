f4112e63885acfc4fb2234c47380152b4c35c8b7
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-09 19:21:08 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -586,7 +586,7 @@ async def tt(ctx):
   try:
     a  = os.popen("git pull").readlines()
     time.sleep(0)
-    await ctx.send(f"```{a}```".replace("['", "").replace("']", "").replace("\n'", " \n").replace(",", "").replace("'", "").replace("\n", " \n"))
+    await ctx.send(f"```{a}```".replace("['", "").replace("']", "").replace("\n'", " \n").replace(",", "").replace("'", "").replace("\n", " \n").replace(".\n'", ""))
   except:
     await ctx.send("Error!")
 

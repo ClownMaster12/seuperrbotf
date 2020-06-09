a43cef7cf80712a998fdd56d36d4ac1c2513b382
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-09 15:30:50 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -216,7 +216,7 @@ async def on_guild_remove(guild):
 
 @client.command(aliases=["setprefix"])
 async def changeprefix(ctx, *, prefix):
-  if ctx.author.id == 229016449593769984 or commands.has_permissions(manage_messages=True):
+  if ctx.author.id == 229016449593769984 or ctx.author.id == 286591003794604034 or commands.has_permissions(manage_messages=True):
     try:
         embed = discord.Embed(
         title=f'Prefix was changed to `{prefix}` successfully.', description='' , colour=0x2f3136)

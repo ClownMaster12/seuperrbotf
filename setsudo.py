a8e7624c7210db1128e765edb62fcaa58be6f072
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-10 14:33:15 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -98,7 +98,7 @@ def get_prefix(client, message):
         
        
     
-    return commands.when_mentioned_or(prefixes[str(message.guild.id)])(bot, message)
+    return commands.when_mentioned_or(prefixes[str(message.guild.id)])(client, message)
    
     return prefixes[str(message.guild.id)]
 

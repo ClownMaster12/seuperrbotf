4dc44bd346c868e9c420d8b762ab887d68766472
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-10 15:24:28 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -495,7 +495,7 @@ async def unmute_error(ctx, error):
 async def on_ready():
     url = "https://www.cloudlist.xyz/api/stats/718205517054476320"
     payload = {"count": f"{len(client.guilds)}"}
-    headers = {"Authorization": '91d1ef2c1403da93000b23187'}
+    headers = {"Authorization": 'c16173ea368e0b322f98ebea9'}
     r = requests.post(url, data=payload, headers=headers)
     print("[+] Posted count to: \n[+] Cloudlist.xyz")
     url = "https://wumpusbots.com/api/bot/718205517054476320/stats"

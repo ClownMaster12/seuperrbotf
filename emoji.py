cddd4f8a8001efa22c5e384786c8a637a9ac079a
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-14 16:57:40 +0100

    Update emoji.py

--- emoji.py
+++ emoji.py
@@ -1,51 +1,42 @@
-
+import random
 import copy
 import datetime
-import random
-import urllib
-import secrets
-import asyncio
-import aiohttp
-import re
 import random
 import asyncio
 from collections import Counter, defaultdict
 import ast
-import dataIO
-import discord
+import psutil
+import platform
+import cpuinfo
+import time
+
+import secrets
+import sys, traceback
+
+import sys, traceback
+import random
+import asyncio
+
 import os
 import asyncio
 import inspect
-from textwrap import dedent
-import http.client
-from secrets import *
-import re
-import requests
-import time
-import html
-import random
-import logging
-import json
-import os
-import re
-import io
-import gc
-import random
-from random import randint
-import time 
-import json
+import copy
 import datetime
-import re
-import time, random
-import random
-import time
-
-import sys, traceback
-
-import sys, traceback
-import discord, time, psutil,json
 import random
 import asyncio
+from collections import Counter, defaultdict
+import ast
+import psutil
+
+import time
+
+import secrets
+import sys, traceback
+
+import sys, traceback
+import random
+import asyncio
+
 import os
 import asyncio
 import inspect
@@ -57,53 +48,26 @@ import time
 import html
 import random
 import ast
+import env
 import sys
 import time
 from collections import Counter, OrderedDict
 from itertools import cycle
 import json
-from discord.ext import commands
-
-
+from datetime import datetime
 import logging
 import asyncio
 import time
 import datetime
-import datetime
-import time
-import random
-import requests
-import json
-import discord
-import os
-import io
-from urllib.parse import quote_plus
-from discord.ext import commands
-import traceback
-import discord
-import aiohttp
 import asyncio
-from webserver import keep_alive
 import os
-from discord.ext.commands import Bot
-
+from discord import Game
+import discord
+from discord.ext import commands
 import praw
-from functools import partial
-from io import BytesIO
-from os.path import join, dirname
-from typing import Union
-
-import discord
-from PIL import Image, ImageDraw, ImageFont
-from colorthief import ColorThief
-from discord.ext import commands
-from pytz import timezone
-import requests
-import discord
-from discord.ext import commands
-import asyncio
-from webserver import keep_alive
-import os
+import platform
+from discord.ext.commands import CommandNotFound
+import subprocess
 
 
 

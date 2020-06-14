87b4ba2d6adfbe6df69626b7041190305a6444ab
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-14 16:55:55 +0100

    Update emoji.py

--- emoji.py
+++ emoji.py
@@ -1,12 +1,116 @@
+
+import copy
+import datetime
+import random
+import urllib
+import secrets
+import asyncio
+import aiohttp
+import re
+import random
+import asyncio
+from collections import Counter, defaultdict
+import ast
+import dataIO
+import discord
+import os
+import asyncio
+import inspect
+from textwrap import dedent
+import http.client
+from secrets import *
+import re
+import requests
+import time
+import html
+import random
+import logging
+import json
+import os
+import re
+import io
+import gc
+import random
+from random import randint
+import time 
+import json
+import datetime
+import re
+import time, random
+import random
+import time
+
+import sys, traceback
+
+import sys, traceback
+import discord, time, psutil,json
+import random
+import asyncio
+import os
+import asyncio
+import inspect
+from textwrap import dedent
+from secrets import *
+import re
+import requests
+import time
+import html
+import random
+import ast
+import sys
+import time
+from collections import Counter, OrderedDict
+from itertools import cycle
+import json
+from discord.ext import commands
+
+
+import logging
+import asyncio
+import time
+import datetime
+import datetime
+import time
+import random
+import requests
+import json
+import discord
+import os
+import io
+from urllib.parse import quote_plus
+from discord.ext import commands
+import traceback
+import discord
+import aiohttp
+import asyncio
+from webserver import keep_alive
+import os
+from discord.ext.commands import Bot
+
+import praw
+from functools import partial
+from io import BytesIO
+from os.path import join, dirname
+from typing import Union
+
+import discord
+from PIL import Image, ImageDraw, ImageFont
+from colorthief import ColorThief
+from discord.ext import commands
+from pytz import timezone
+import requests
 import discord
 from discord.ext import commands
-import requests
 import asyncio
-import env
-import json
+from webserver import keep_alive
+import os
 
-#Variables
-client = commands.client(command_prefix='-')
+
+
+client=discord.Client()
+client=discord.Client(shard_count=1,shard_id=1)
+client = commands.Bot(command_prefix = "img ")
+client.remove_command('help')
 
   
 @client.event

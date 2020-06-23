04f128246782f83666bfe97a808676af1c5037b4
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-23 23:37:26 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -411,6 +411,54 @@ async def dstatus(ctx):
         await m.edit(embed=embed)
     
     
+import wikipedia
+import os
+from chatbot import Chat, register_call	
+			
+			
+
+
+
+@register_call("whoIs")
+def who_is(query, session_id="general"):
+    try:
+        return wikipedia.summary(query)
+    except Exception:
+        for new_query in wikipedia.search(query):
+            try:
+                return wikipedia.summary(new_query)
+            except Exception:
+                pass
+    return "I don't know about "+query
+template_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"chatbotTemplate","chatbottemplate.template")
+chat=Chat(template_file_path)
+@client.command(pass_context = True, aliases=["chat", "ai"])
+async def chatbot(ctx,*,message):
+    embed = discord.Embed(
+        title=f'Processing..', description='' , colour=0x2f3136)
+    embed.set_footer(text="If this is still showing run the command again.")
+
+
+    m = await ctx.send(embed = embed)
+    result = chat.respond(message)
+    if(len(result)<=2048):
+        embed=discord.Embed(title="ChatBot", description = result, color = (0x2f3136))
+        await m.edit(embed=embed)
+    else:
+        embedList = []
+        n=2048
+        embedList = [result[i:i+n] for i in range(0, len(result), n)]
+        for num, item in enumerate(embedList, start = 1):
+            if(num == 1):
+                embed = discord.Embed(title="ChatBot", description = item, color = (0x2f3136))
+                embed.set_footer(text="Page {}".format(num))
+                await m.edit(embed = embed)
+            else:
+                embed = discord.Embed(description = item, color = (0x2f3136))
+                embed.set_footer(text = "Page {}".format(num))
+                await m.edit(embed = embed)
+			
+			
     
 @client.event
 async def on_message(message):

ebf0c83dcbdef620318f6a91d5a265d0003526a0
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-24 14:06:16 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -646,7 +646,7 @@ async def aqua(ctx: commands.Context):
         j = r.json()
 
 
-        dj = f'{j["live"]["streamer_name"]}'
+        dj = f'{str(datetime.timedelta(seconds=j["now_playing"]["duration"]))}'.replace("0:00:00", f'{j["live"]["streamer_name"]}')
         
 
 
@@ -655,7 +655,7 @@ async def aqua(ctx: commands.Context):
         embed.add_field(name="> Listeners", value=f"Total: `{j['listeners']['total']}`\nUnique: `{j['listeners']['unique']}`\n", inline=False)
         embed.add_field(name="> Now Playing", value=f'{j["now_playing"]["song"]["text"]} `[{str(datetime.timedelta(seconds=j["now_playing"]["duration"]))}]`'.replace("0:00:00", f'🔴 Live'), inline=False)
         embed.add_field(name="> Up Next", value=f'{j["playing_next"]["song"]["text"]} `[{str(datetime.timedelta(seconds=j["playing_next"]["duration"]))}]`', inline=False)
-        embed.add_field(name="> DJ", value=f'{dj}', inline=False)
+        embed.add_field(name="> DJ", value=f'{dj}'.replace(f"{str(datetime.timedelta(seconds=j["now_playing"]["duration"]))}", f'AutoDJ'), inline=False)
 
 
         await m.edit(embed=embed)

eed191727ff580d1b0a11b17642dce7af3b019cc
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-25 15:52:53 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -748,7 +748,7 @@ async def aqua(ctx: commands.Context):
         embed.add_field(name="> Listeners", value=f"Total: `{j['listeners']['total']}`\nUnique: `{j['listeners']['unique']}`\n", inline=False)
         embed.add_field(name="> Now Playing", value=f'{j["now_playing"]["song"]["text"]} `[{str(datetime.timedelta(seconds=j["now_playing"]["duration"]))}]`'.replace("0:00:00", f'🔴 Live'), inline=False)
         embed.add_field(name="> Up Next", value=f'{j["playing_next"]["song"]["text"]} `[{str(datetime.timedelta(seconds=j["playing_next"]["duration"]))}]`', inline=False)
-	embed.set_image(url=f"{img}")
+	embed.set_thumbnail(url = f"{img}")
         embed.add_field(name="> DJ", value=f'{dj}'.replace(f'{str(datetime.timedelta(seconds=j["now_playing"]["duration"]))}', f'AutoDJ'), inline=False)
 							   
 

55e7ae0886d9c7c33d4afeb650d70e8b193ba443
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-25 22:03:11 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -715,12 +715,11 @@ async def background_task():
     channel = client.get_channel(725811911291306095) # Insert channel ID here
     while not client.is_closed():
 	    r = requests.get(f'https://api.itsaqua.net/v1/all')
-            j = r.json()
+        j = r.json()
 	    embed=discord.Embed(title=f"Aqua Now Playing", description=f'{j["nowplaying"]["title"]} - {j["nowplaying"]["artist"]}')
 	    embed.set_thumbnail(url=f"{j['images']['album']}")
-            await channel.send(embed=embed)
-            await asyncio.sleep(j["nowplaying"]["total"])
-
+        await channel.send(embed=embed)
+        await asyncio.sleep(j["nowplaying"]["total"])
 
 
 @client.command()

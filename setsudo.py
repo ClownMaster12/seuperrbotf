9903cdf7e434d30e8c42767b66205c3f5c7d8de2
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-25 17:32:09 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -741,7 +741,7 @@ async def aqua(ctx: commands.Context):
       m = await ctx.send(embed=embed)
       time.sleep(0)
       try:
-        dj = f'{str(datetime.timedelta(seconds=j["now_playing"]["duration"]))}'.replace("0:00:00", f'{j["live"]["streamer_name"]}')
+        dj = f'{h['presenter']['name']}'
         
         img = f"{h['images']['album']}"
 
@@ -752,7 +752,7 @@ async def aqua(ctx: commands.Context):
         embed.add_field(name="> Now Playing", value=f'{j["now_playing"]["song"]["text"]} `[{str(datetime.timedelta(seconds=j["now_playing"]["duration"]))}]`'.replace("0:00:00", f'🔴 Live'), inline=False)
         embed.add_field(name="> Up Next", value=f'{j["playing_next"]["song"]["text"]} `[{str(datetime.timedelta(seconds=j["playing_next"]["duration"]))}]`', inline=False)
 
-        embed.add_field(name="> DJ", value=f'{dj}'.replace(f'{str(datetime.timedelta(seconds=j["now_playing"]["duration"]))}', f'AutoDJ'), inline=False)
+        embed.add_field(name="> DJ", value=f'{dj}'), inline=False)
 							   
 
 

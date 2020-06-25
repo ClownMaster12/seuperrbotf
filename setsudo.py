d9d962adb7ddd4eb2f442a4ea1dc262e430857f1
ClownMaster12 <66507715+ClownMaster12@users.noreply.github.com>
2020-06-25 21:48:45 +0100

    Update setsudo.py

--- setsudo.py
+++ setsudo.py
@@ -750,20 +750,6 @@ async def aqua(ctx: commands.Context):
         await m.edit(embed=embed)
 
 
-async def background_task():
-    await client.wait_until_ready()
-    r = requests.get(f'https://api.itsaqua.net/v1/all')
-    j = r.json()	
-    channel = client.get_channel(725811911291306095) # Insert channel ID here
-    while not client.is_closed():	
-                     	img = f"{j['images']['album']}"
-	
-	                embed = discord.Embed(title=f"Aqua Now Playing", description=f'{j["nowplaying"]["title"]} - {j["nowplaying"]["artist"]}')
-	                embed.set_thumbnail(url=f"{img}")
-							  
-
-                        await channel.send(embed=embed)
-                        await asyncio.sleep(j["nowplaying"]["total"])	
 			
 
 	
@@ -1476,6 +1462,7 @@ async def _eval_error(ctx, error):
 
         
 
-client.loop.create_task(background_task())
+
+		    
 client.loop.create_task(update_stats())
 client.run(env.TOKEN)
